73301ad8e9dd85867bd377ef7201a7eae11bd6b6
frsw3nr <frsw3nr@msn.com>
2020-12-23 09:57:44 +0900

    g1.20u

--- exp/vm/vmwareconf/vmware.go
+++ exp/vm/vmwareconf/vmware.go
@@ -61,35 +61,23 @@ func (e *VMWare) retrieveInventory(env *cfg.RunEnv, ioErr io.Writer, vm string) 
 		e.saveJson(ioErr, objectId, query)
 	}
 	// Default extraction
-	e.saveJson(ioErr, "base_hardware",
-		"Config.Hardware")
-	e.saveJson(ioErr, "base_cpu_memory_resource",
-		"{ResourceConfig.CpuAllocation,ResourceConfig.MemoryAllocation}")
-	e.saveJson(ioErr, "base_datastore",
-		"Storage.PerDatastoreUsage")
-	e.saveJson(ioErr, "base_disk",
-		"Guest.Disk")
-	e.saveJson(ioErr, "base_boot",
-		"Config.BootOptions")
-	e.saveJson(ioErr, "base_ipstack",
-		"Guest.IpStack")
-	e.saveJson(ioErr, "base_net",
-		"Guest.Net")
-	e.saveJson(ioErr, "base_ext_config",
-		"Config.ExtraConfig")
+	// e.saveJson(ioErr, "base_hardware",
+	// 	"Config.Hardware")
+	// e.saveJson(ioErr, "base_cpu_memory_resource",
+	// 	"{ResourceConfig.CpuAllocation,ResourceConfig.MemoryAllocation}")
+	// e.saveJson(ioErr, "base_datastore",
+	// 	"Storage.PerDatastoreUsage")
+	// e.saveJson(ioErr, "base_disk",
+	// 	"Guest.Disk")
+	// e.saveJson(ioErr, "base_boot",
+	// 	"Config.BootOptions")
+	// e.saveJson(ioErr, "base_ipstack",
+	// 	"Guest.IpStack")
+	// e.saveJson(ioErr, "base_net",
+	// 	"Guest.Net")
+	// e.saveJson(ioErr, "base_ext_config",
+	// 	"Config.ExtraConfig")
 }
-
-// func HandleError(w io.Writer, inErr error, message string) error {
-// 	if inErr != nil {
-// 		_, err := fmt.Fprintf(w, "%s : %s\n", message, inErr)
-// 		if err != nil {
-// 			log.Errorf("write log error : %s", err)
-// 		}
-// 		return errors.Wrap(inErr, message)
-// 	} else {
-// 		return nil
-// 	}
-// }
 
 func (e *VMWare) Run(ctx context.Context, env *cfg.RunEnv) error {
 	startTime := time.Now()
@@ -177,8 +165,6 @@ func (e *VMWare) Run(ctx context.Context, env *cfg.RunEnv) error {
 	if err = res.collectReferences(pc, ctx); err != nil {
 		return HandleError(errFile, err, "get vm info")
 	}
-	fmt.Printf("Reference:%v\n", res.entities)
-
 	// 仮想インスタンス情報を出力
 	if len(e.Servers) == 0 && len(vms) != 1 {
 		return HandleError(errFile, err, fmt.Sprintf("vm not found '%s'", e.Server))
@@ -194,130 +180,25 @@ func (e *VMWare) Run(ctx context.Context, env *cfg.RunEnv) error {
 		if err := RemoveAndCreateDir(e.datastore); err != nil {
 			return HandleError(errFile, err, "create log directory")
 		}
+
+		hostName := "N/A"
+		if href := vm.Summary.Runtime.Host; href != nil {
+			if name, ok := res.entities[*href]; ok {
+				hostName = name
+			}
+		}
+		hostPath := filepath.Join(e.datastore, "host.txt")
+		if err := ioutil.WriteFile(hostPath, []byte(hostName), 0666); err != nil {
+			return HandleError(errFile, err, "write vm host info")
+		}
 		alldataPath := filepath.Join(e.datastore, "all")
 		if err := ioutil.WriteFile(alldataPath, bytes, 0666); err != nil {
 			return HandleError(errFile, err, "write vm all info json")
 		}
 		e.json = string(bytes)
-
-		// fmt.Printf("Summary:%v\n", gjson.Get(e.json, "Summary").String())
-		// fmt.Printf("Host:%v\n", gjson.Get(e.json, "Runtime").String())
-
 		e.retrieveInventory(env, errFile, e.vmName)
 	}
 	log.Infof("retrieve vm : %d, elapse %s", len(vms), time.Since(startTime))
 
 	return nil
 }
-
-// type info struct {
-// 	WaitForIP       bool
-// 	General         bool
-// 	ExtraConfig     bool
-// 	Resources       bool
-// 	ToolsConfigInfo bool
-// }
-
-// type infoResult struct {
-// 	VirtualMachines []mo.VirtualMachine
-// 	objects         []*object.VirtualMachine
-// 	entities        map[types.ManagedObjectReference]string
-// 	cmd             *info
-// }
-
-// // collectReferences builds a unique set of MORs to the set of VirtualMachines,
-// // so we can collect properties in a single call for each reference type {host,datastore,network}.
-// func (r *infoResult) collectReferences(pc *property.Collector, ctx context.Context) error {
-// 	// MOR -> Name map
-// 	r.entities = make(map[types.ManagedObjectReference]string)
-
-// 	var host []mo.HostSystem
-// 	var network []mo.Network
-// 	var opaque []mo.OpaqueNetwork
-// 	var dvp []mo.DistributedVirtualPortgroup
-// 	var datastore []mo.Datastore
-// 	// Table to drive inflating refs to their mo.* counterparts (dest)
-// 	// and save() the Name to r.entities w/o using reflection here.
-// 	// Note that we cannot use a []mo.ManagedEntity here, since mo.Network has its own 'Name' field,
-// 	// the mo.Network.ManagedEntity.Name field will not be set.
-// 	vrefs := map[string]*struct {
-// 		dest interface{}
-// 		refs []types.ManagedObjectReference
-// 		save func()
-// 	}{
-// 		"HostSystem": {
-// 			&host, nil, func() {
-// 				for _, e := range host {
-// 					r.entities[e.Reference()] = e.Name
-// 				}
-// 			},
-// 		},
-// 		"Network": {
-// 			&network, nil, func() {
-// 				for _, e := range network {
-// 					r.entities[e.Reference()] = e.Name
-// 				}
-// 			},
-// 		},
-// 		"OpaqueNetwork": {
-// 			&opaque, nil, func() {
-// 				for _, e := range opaque {
-// 					r.entities[e.Reference()] = e.Name
-// 				}
-// 			},
-// 		},
-// 		"DistributedVirtualPortgroup": {
-// 			&dvp, nil, func() {
-// 				for _, e := range dvp {
-// 					r.entities[e.Reference()] = e.Name
-// 				}
-// 			},
-// 		},
-// 		"Datastore": {
-// 			&datastore, nil, func() {
-// 				for _, e := range datastore {
-// 					r.entities[e.Reference()] = e.Name
-// 				}
-// 			},
-// 		},
-// 	}
-
-// 	xrefs := make(map[types.ManagedObjectReference]bool)
-// 	// Add MOR to vrefs[kind].refs avoiding any duplicates.
-// 	addRef := func(refs ...types.ManagedObjectReference) {
-// 		for _, ref := range refs {
-// 			if _, exists := xrefs[ref]; exists {
-// 				return
-// 			}
-// 			xrefs[ref] = true
-// 			vref := vrefs[ref.Type]
-// 			vref.refs = append(vref.refs, ref)
-// 		}
-// 	}
-
-// 	for _, vm := range r.VirtualMachines {
-// 		if r.cmd.General {
-// 			if ref := vm.Summary.Runtime.Host; ref != nil {
-// 				addRef(*ref)
-// 			}
-// 		}
-
-// 		if r.cmd.Resources {
-// 			addRef(vm.Datastore...)
-// 			addRef(vm.Network...)
-// 		}
-// 	}
-
-// 	for _, vref := range vrefs {
-// 		if vref.refs == nil {
-// 			continue
-// 		}
-// 		err := pc.Retrieve(ctx, vref.refs, []string{"name"}, vref.dest)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		vref.save()
-// 	}
-
-// 	return nil
-// }
